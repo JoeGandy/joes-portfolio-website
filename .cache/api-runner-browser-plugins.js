7ce6b744607463e8a5e22f1540e0e7b823de7c1b
Joe Gandy <joeg@adaptive.co.uk>
2019-02-14 10:43:35 +0000

    Removing .cache directory files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/joe/misc/joes-portfolio-website/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"UA-49984946-7"},
-    },{
-      plugin: require('/home/joe/misc/joes-portfolio-website/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
